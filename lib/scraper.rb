ca4817b59a0e87428a8d5a16bfd3fcac6c12611e
Anysia V <anysiavalko@gmail.com>
2019-02-18 02:30:15 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -35,15 +35,15 @@ class Scraper
       url = url.attr("href")
       if url.include?("twitter")
         twitter_url = url
-        student_info[:twitter] = twitter_url 
+        student_info[:twitter] = twitter_url #if twitter_url
       elsif url.include?("linkedin")
         linkedin_url = url
-        student_info[:linkedin] = linkedin_url 
+        student_info[:linkedin] = linkedin_url #if linkedin_url
       elsif url.include?("github")
          github_url = url
-        student_info[:github] = github_url 
+        student_info[:github] = github_url #if github_url
       else blog_url = url
-        student_info[:blog] = blog_url 
+        student_info[:blog] = blog_url #if blog_url
       end  
     end
     profile_quote = doc.css("div.profile-quote").text
@@ -52,6 +52,10 @@ class Scraper
     student_info[:bio] = bio if bio 
     student_info
   end   
+    
+  #First, the url variables weren't being recognized outside of the iteration, so I assigned them to nil beforehand. But then nil was showing up as values in the hash, so I assigned the key value pairs on separate lines with if statements. But when I moved the key value assignments into the iteration, I didn't need the nils anymore, so then I didn't need the if statements anymore.
+    
+    
     
 end
 
